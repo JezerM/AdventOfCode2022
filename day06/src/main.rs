391df36e600c99a75cc13c0b318cdccd13cd4396
Jezer Mejía <amyuki4@icloud.com>
2023-02-03 23:50:56 -0600

    Improve day06 and implement tests

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -29,22 +29,45 @@ fn find_marker(string: &String, marker_length: usize) -> u32 {
     return 0;
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    fn do_test(marker_length: usize, expected: u32) {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+
+        let mut sequence = String::new();
+        let mut sum_of_markers = 0;
+        for c in input_contents.chars() {
+            if c == '\n' {
+                let marker = find_marker(&sequence, marker_length);
+                sum_of_markers += marker;
+                sequence.clear();
+                continue;
+            }
+            sequence.push(c);
+        }
+        assert_eq!(sum_of_markers, expected);
+    }
+
+    #[test]
+    fn part_one() {
+        do_test(4, 39);
+    }
+    #[test]
+    fn part_two() {
+        do_test(14, 120);
+    }
+}
+
 fn main() {
     let input_contents =
         fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
 
-    let mut sequence = String::new();
+    let marker = find_marker(&input_contents, 4);
+    println!("Marker (one) found at: {}", marker);
 
-    for c in input_contents.chars() {
-        if c == '\n' {
-            let marker = find_marker(&sequence, 4);
-            println!("Marker (one) found at: {}", marker);
-
-            let marker = find_marker(&sequence, 14);
-            println!("Marker (two) found at: {}", marker);
-            sequence.clear();
-            continue;
-        }
-        sequence.push(c);
-    }
+    let marker = find_marker(&input_contents, 14);
+    println!("Marker (two) found at: {}", marker);
 }
