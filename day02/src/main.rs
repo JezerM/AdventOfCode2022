9ac0a7130b9b35ae0b2fe094c99c2e26d12d0bed
Jezer Mejía <amyuki4@icloud.com>
2023-02-03 19:45:58 -0600

    Improve day02 and implement tests

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,14 +1,13 @@
 use std::fs;
 
-#[derive(PartialEq, Eq, PartialOrd, Debug, Clone)]
-pub enum Choice {
+#[derive(PartialEq, Clone)]
+enum Choice {
     Rock,
     Paper,
     Scissors,
 }
 
-#[derive(Debug)]
-pub enum EndChoice {
+enum EndChoice {
     Lose,
     Draw,
     Win,
@@ -59,21 +58,10 @@ fn calculate_score(my_choice: &Choice, opp_choice: &Choice) -> u32 {
     }
 
     score += match game_state {
-        GameState::Draw => {
-            println!("Draw - {:?} - {:?}", my_choice, opp_choice);
-            3
-        }
-        GameState::Win => {
-            println!("Win - {:?} - {:?}", my_choice, opp_choice);
-            6
-        }
-        GameState::Lose => {
-            println!("Lose - {:?} - {:?}", my_choice, opp_choice);
-            0
-        }
+        GameState::Draw => 3,
+        GameState::Win => 6,
+        GameState::Lose => 0,
     };
-
-    println!("\tSum score: {score}");
 
     return score;
 }
@@ -100,28 +88,47 @@ fn calculate_choice_and_score(how_to_end: &EndChoice, opp_choice: &Choice) -> u3
             my_choice = get_winner_choice(&opp_choice);
         }
     }
-    println!("Choice: {:?} - {:?}", how_to_end, my_choice);
 
     return calculate_score(&my_choice, opp_choice);
 }
 
-fn main() {
-    let input_contents =
-        fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
+fn get_score(input: &String) -> u32 {
+    let mut my_choice: Option<Choice> = None;
+    let mut opp_choice: Option<Choice> = None;
 
+    let mut score: u32 = 0;
+
+    for c in input.chars() {
+        if c == '\n' {
+            if my_choice.is_none() || opp_choice.is_none() {
+                continue;
+            }
+            score += calculate_score(&my_choice.unwrap(), &opp_choice.unwrap());
+            my_choice = None;
+            opp_choice = None;
+            continue;
+        }
+        if opp_choice.is_none() {
+            opp_choice = char_to_choice(c);
+        } else if my_choice.is_none() {
+            my_choice = char_to_choice(c);
+        }
+    }
+    return score;
+}
+fn get_score_choice(input: &String) -> u32 {
     let mut my_choice: Option<Choice> = None;
     let mut opp_choice: Option<Choice> = None;
     let mut end_choice: Option<EndChoice> = None;
 
     let mut score: u32 = 0;
 
-    for c in input_contents.chars() {
+    for c in input.chars() {
         if c == '\n' {
             if my_choice.is_none() || opp_choice.is_none() {
                 continue;
             }
-            score += calculate_score(&my_choice.unwrap(), &opp_choice.unwrap());
-            //score += calculate_choice_and_score(&end_choice.unwrap(), &opp_choice.unwrap());
+            score += calculate_choice_and_score(&end_choice.unwrap(), &opp_choice.unwrap());
             my_choice = None;
             opp_choice = None;
             end_choice = None;
@@ -134,6 +141,37 @@ fn main() {
             my_choice = char_to_choice(c);
         }
     }
+    return score;
+}
 
-    println!("Score: {score}");
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn part_one() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+
+        let score = get_score(&input_contents);
+        assert_eq!(score, 15);
+    }
+    #[test]
+    fn part_two() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+
+        let score = get_score_choice(&input_contents);
+        assert_eq!(score, 12);
+    }
+}
+
+fn main() {
+    let input_contents =
+        fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
+
+    let score_first = get_score(&input_contents);
+    let score_second = get_score_choice(&input_contents);
+
+    println!("Score (part 1): {}", score_first);
+    println!("Score (part 2): {}", score_second);
 }
