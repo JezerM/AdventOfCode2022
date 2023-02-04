716ecb54cf2fff6d60c7bd95593db4013f9c0a57
Jezer Mejía <amyuki4@icloud.com>
2023-02-03 19:25:33 -0600

    Improved day01 code and implemented tests

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,53 +1,85 @@
-use std::env;
 use std::fs;
+
+fn iterate_input(input: &String, mut f: impl FnMut(u32) -> ()) {
+    let mut curr_calories = 0;
+
+    let mut word = String::new();
+
+    for c in input.chars() {
+        if c == '\n' {
+            if !word.is_empty() {
+                curr_calories += word.parse::<u32>().unwrap();
+                word.clear();
+                continue;
+            }
+            f(curr_calories);
+            curr_calories = 0;
+            word.clear();
+            continue;
+        }
+        word.push(c);
+    }
+    f(curr_calories);
+}
+
+fn get_max_calories(input: &String) -> u32 {
+    let mut max_calories: u32 = 0;
+
+    iterate_input(input, |curr_calories| {
+        if curr_calories >= max_calories {
+            max_calories = curr_calories;
+        }
+    });
+    return max_calories;
+}
+
+fn get_top_calories(input: &String) -> [u32; 3] {
+    let mut top_calories: [u32; 4] = [0; 4];
+
+    iterate_input(input, |curr_calories| {
+        top_calories[3] = curr_calories;
+        top_calories.sort_by(|a, b| b.cmp(a));
+    });
+    let mut result = [0; 3];
+    for i in 0..3 {
+        result[i] = top_calories[i];
+    }
+
+    return result;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn max_calories() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+
+        let max_calories = get_max_calories(&input_contents);
+        assert_eq!(max_calories, 24000);
+    }
+    #[test]
+    fn top_calories() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+
+        let top_calories = get_top_calories(&input_contents);
+
+        let top_calories_sum: u32 = top_calories.iter().sum();
+        assert_eq!(top_calories_sum, 45000);
+    }
+}
 
 fn main() {
     let input_contents =
         fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
 
-    let mut top_calories: [i32; 4] = [0; 4];
+    let max_calories = get_max_calories(&input_contents);
+    let top_calories = get_top_calories(&input_contents);
 
-    //let mut max_calories: i32 = 0;
-    let mut curr_calories: i32 = 0;
-    let mut is_word: bool = true;
+    let top_calories_sum: u32 = top_calories.iter().sum();
 
-    let mut word: String = String::new();
-    for c in input_contents.chars() {
-        if c == '\n' && is_word {
-            is_word = false;
-            println!("Word: {word}");
-            let calories = word.parse::<i32>().unwrap();
-            word.clear();
-
-            curr_calories += calories;
-        } else if c == '\n' && !is_word {
-            println!("Read elf: {curr_calories}");
-            top_calories[3] = curr_calories;
-            top_calories.sort_by(|a, b| b.cmp(a));
-            //if curr_calories >= max_calories {
-            //max_calories = curr_calories;
-            //}
-            curr_calories = 0;
-        } else {
-            is_word = true;
-            word.push(c);
-        }
-    }
-    println!("Read elf: {curr_calories}");
-    top_calories[3] = curr_calories;
-    top_calories.sort_by(|a, b| b.cmp(a));
-
-    println!("Top calories: {:?}", top_calories);
-
-    let mut final_calories = 0;
-    for n in 0..3 {
-        final_calories += top_calories[n];
-    }
-
-    println!("Final calories: {final_calories}");
-
-    //if curr_calories >= max_calories {
-    //max_calories = curr_calories;
-    //}
-    //println!("Max calories: {max_calories}");
+    println!("Max calories (part 1): {}", max_calories);
+    println!("Top calories sum (part 2): {}", top_calories_sum);
 }
