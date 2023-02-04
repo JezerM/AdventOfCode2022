9bd10ab1d1a9fa85c5ebcd509a59ba3cb0acdb59
Jezer Mejía <amyuki4@icloud.com>
2023-02-03 23:42:04 -0600

    Improve day05 and implement tests

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -44,13 +44,37 @@ fn move_crates_at_once(crate_stacks: &mut Vec<Vec<char>>, quantity: usize, from:
     to_stack.append(&mut stack);
 }
 
-fn main() {
-    let input_contents =
-        fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
-
+fn get_crate_stacks(input: &String) -> Vec<Vec<char>> {
     let mut crate_stacks: Vec<Vec<char>> = Vec::new();
     let mut stack_index = 0;
 
+    for (i, c) in input.char_indices() {
+        if (i + 1) % 2 == 0 && (i + 1) % 4 != 0 {
+            if c.is_digit(10) {
+                reverse_stacks(&mut crate_stacks);
+                break;
+            }
+            let mut curr_stack = crate_stacks.get_mut(stack_index);
+            if curr_stack.is_none() {
+                crate_stacks.push(Vec::new());
+            }
+            curr_stack = crate_stacks.get_mut(stack_index);
+
+            if c != ' ' {
+                curr_stack.unwrap().push(c);
+            }
+
+            stack_index += 1;
+            continue;
+        }
+        if c == '\n' {
+            stack_index = 0;
+        }
+    }
+    return crate_stacks;
+}
+
+fn do_crate_movements(input: &String, crate_stacks: &mut Vec<Vec<char>>, multiple_movement: bool) {
     let mut word = String::new();
     let mut move_quantity = 0;
     let mut move_from = 0;
@@ -58,67 +82,93 @@ fn main() {
     let mut move_env = MoveEnv::None;
 
     let mut reading_stack = true;
-
-    for (i, c) in input_contents.char_indices() {
+    for c in input.chars() {
         if reading_stack {
-            if (i + 1) % 2 == 0 && (i + 1) % 4 != 0 {
-                if c.is_digit(10) {
-                    reading_stack = false;
-                    reverse_stacks(&mut crate_stacks);
-                    continue;
-                }
-
-                let mut curr_stack = crate_stacks.get_mut(stack_index);
-                if curr_stack.is_none() {
-                    crate_stacks.push(Vec::new());
-                }
-                curr_stack = crate_stacks.get_mut(stack_index);
-
-                if c != ' ' {
-                    curr_stack.unwrap().push(c);
-                }
-
-                stack_index += 1;
-                continue;
+            if c.is_digit(10) {
+                reading_stack = false;
             }
-            if c == '\n' {
-                stack_index = 0;
-            }
-        } else {
-            if c.is_whitespace() {
-                match (word.parse::<usize>(), &move_env) {
-                    (Ok(number), MoveEnv::Move) => {
-                        move_quantity = number;
-                    }
-                    (Ok(number), MoveEnv::From) => {
-                        move_from = number - 1;
-                    }
-                    (Ok(number), MoveEnv::To) => {
-                        move_to = number - 1;
-                    }
-                    _ => {}
-                }
-                move_env = match word.as_str() {
-                    "move" => MoveEnv::Move,
-                    "from" => MoveEnv::From,
-                    "to" => MoveEnv::To,
-                    _ => MoveEnv::None,
-                };
-                word.clear();
-                if c != '\n' {
-                    continue;
-                }
-                //move_crates(&mut crate_stacks, move_quantity, move_from, move_to);
-                move_crates_at_once(&mut crate_stacks, move_quantity, move_from, move_to);
-                word.clear();
-                continue;
-            }
-            word.push(c);
+            continue;
         }
+        if !c.is_whitespace() {
+            word.push(c);
+            continue;
+        }
+
+        match (word.parse::<usize>(), &move_env) {
+            (Ok(number), MoveEnv::Move) => {
+                move_quantity = number;
+            }
+            (Ok(number), MoveEnv::From) => {
+                move_from = number - 1;
+            }
+            (Ok(number), MoveEnv::To) => {
+                move_to = number - 1;
+            }
+            _ => {}
+        }
+        move_env = match word.as_str() {
+            "move" => MoveEnv::Move,
+            "from" => MoveEnv::From,
+            "to" => MoveEnv::To,
+            _ => MoveEnv::None,
+        };
+        word.clear();
+        if c != '\n' {
+            continue;
+        }
+        if multiple_movement {
+            move_crates_at_once(crate_stacks, move_quantity, move_from, move_to);
+        } else {
+            move_crates(crate_stacks, move_quantity, move_from, move_to);
+        }
+        word.clear();
     }
+}
 
-    let top_stack: Vec<char> = get_top_stack(&crate_stacks);
-    let top: String = top_stack.iter().collect();
+fn get_top_stack_one(input: &String) -> String {
+    let mut crate_stacks = get_crate_stacks(input);
+    do_crate_movements(input, &mut crate_stacks, false);
 
-    println!("{}", top);
+    let top_stack = get_top_stack(&crate_stacks);
+    return top_stack.iter().collect();
+}
+fn get_top_stack_multiple(input: &String) -> String {
+    let mut crate_stacks = get_crate_stacks(input);
+    do_crate_movements(input, &mut crate_stacks, true);
+
+    let top_stack = get_top_stack(&crate_stacks);
+    return top_stack.iter().collect();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+
+    #[test]
+    fn part_one() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+        let top_stack = get_top_stack_one(&input_contents);
+        assert_eq!(top_stack, "CMZ");
+    }
+    #[test]
+    fn part_two() {
+        let input_contents = fs::read_to_string("./test.txt").expect("Expected test file");
+        let top_stack = get_top_stack_multiple(&input_contents);
+        assert_eq!(top_stack, "MCD");
+    }
+}
+
+fn main() {
+    let input_contents =
+        fs::read_to_string("./input.txt").expect("Expected input file at input.txt");
+
+    let top_stack_one = get_top_stack_one(&input_contents);
+    let top_stack_multiple = get_top_stack_multiple(&input_contents);
+
+    println!("Top stack with CrateMover 9000 (part 1): {}", top_stack_one);
+    println!(
+        "Top stack with CrateMover 9001, multiple (part 2): {}",
+        top_stack_multiple
+    );
 }
